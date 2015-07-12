734706bd1ecc537b2bfd6869b2f12d753f80f0b0
Christopher Miller <cmill4ed@mail.umw.edu>
2015-07-12 19:34:51 -0400

    mkdir

--- index.js
+++ index.js
@@ -7,7 +7,34 @@ const PLUGIN_NAME = 'gulp-webdav-sync'
 
 module.exports = function ( i ) {
   return through2.obj( function ( vinyl, encoding, callback ) {
-    gutil.log( vinyl.path, vinyl.isNull() )
-    return callback( null, vinyl )
+    var uri = i + vinyl.relative
+    this.pause()
+    if ( vinyl.isNull() ) {
+      mkdir( uri, function ( res ) {
+        gutil.log(
+          uri
+          , res.statusCode
+          , http.STATUS_CODES[res.statusCode]
+        )
+        callback()
+      } )
+      this.resume()
+      return
+    }
+    callback( null, vinyl )
+    this.resume()
   } )
+}
+
+function mkdir( uri, callback ) {
+  var options, req
+  options = url.parse( uri )
+  options.method = 'MKCOL'
+  req = http.request( options, function ( res ) {
+    callback( res )
+  } )
+  req.on( 'error', function ( e ) {
+    throw new gutil.PluginError( PLUGIN_NAME, e.toString() )
+  } )
+  req.end()
 }
