f971f3013965870808f7b3bfacc67d49ff31ac40
Christopher Miller <cmill4ed@mail.umw.edu>
2015-08-10 03:15:21 -0400

    extraneous require

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ var path = require( 'path' )
 var Stream = require( 'stream' )
 var underscore = require( 'underscore' )
 var url = require( 'url' )
-var xml2js = require( 'xml2js' )
 
 const PLUGIN_NAME = 'gulp-webdav-sync'
 var stream
