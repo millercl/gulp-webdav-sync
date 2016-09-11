9f8fbd75cb01e70b5734d35da1dff073c1235197
Christopher Miller <cmill4ed@mail.umw.edu>
2016-09-11 13:25:26 -0400

    Moves multi-status response object translation to sub-module.

--- index.js
+++ index.js
@@ -35,6 +35,7 @@ if ( !Object.assign ) {
     , writable: true
   } )
 }
+var rfc2518 = require( './lib/rfc2518' )
 var url = require( 'url' )
 var xml2js = require( 'xml2js' )
 
@@ -148,7 +149,7 @@ module.exports = function () {
     if ( _options.list === 'dest' ) {
       _propfind( dest_url, 0, _options, function ( res, dom ) {
         if ( res.statusCode === 207 ) {
-          dest_propfind = _xml_parse( dom )[0]
+          dest_propfind = rfc2518.tr_207( dom )[0]
           log.var( '$dest_propfind' )
           log.var( ' .getcontentlength', dest_propfind.getcontentlength )
           log.var( ' .getlastmodified', dest_propfind.getlastmodified )
@@ -645,49 +646,6 @@ function _xml_to_url_a( dom ) {
   function href( element ) {
     return element.href
   }
-  return _xml_parse( dom ).map( href )
+  return rfc2518.tr_207( dom ).map( href )
 }
 
-function _xml_parse( dom ) {
-  try {
-    return dom.multistatus.response.map( function ( response ) {
-      var href = response.href[0]
-      var propstat = response.propstat[0]
-      var prop = response.propstat[0].prop[0]
-      var getlastmodified = response.propstat[0].prop[0].getlastmodified[0]
-      var stat = response.propstat[0].status[0]
-      var resource = {}
-      resource.href = href._
-      if ( propstat ) {
-        if ( prop ) {
-          var getcontentlength = response.propstat[0].prop[0].getcontentlength
-          if ( getcontentlength ) {
-            resource.getcontentlength = getcontentlength[0]._
-          }
-          if ( getlastmodified ) {
-            resource.getlastmodified = new Date( getlastmodified._ )
-          }
-          var resourcetype = response.propstat[0].prop[0].resourcetype
-          if ( resourcetype ) {
-            if ( typeof resourcetype[0] === 'string' ) {
-              resource.resourcetype = null
-            } else
-            if ( typeof resourcetype[0] === 'object' ) {
-              resource.resourcetype = Object.keys( resourcetype[0] )[0]
-            }
-          }
-        }
-        if ( stat ) {
-          resource.stat = http_status_to_int( stat._ )
-        }
-      }
-      return resource
-    } )
-  } catch ( error ) {
-    _on_error( error )
-  }
-  return []
-  function http_status_to_int( string ) {
-    return Number( /\d{3}/.exec( string ) )
-  }
-}
